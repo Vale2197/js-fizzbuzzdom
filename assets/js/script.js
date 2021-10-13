62ce481afc2224ff8ab068e003ab288849a2a148
Vale2197 <valentinatrebbi97@gmail.com>
2021-10-13 16:44:18 +0200

    completato ciclo con condizioni, aggiunto titolo da script.js
    e aggiunto stile all intera pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,31 @@
 /* Scrivi un programma che esegua un ciclo da 1 a 100 e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell'indice
 per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5. */
 const list = document.querySelector(".numberList");
-const listItem = document.createElement("li");
-const numero = list.appendChild(listItem);
+const title = document.createElement("h1");
 
 //Scrivi un programma che esegua un ciclo da 1 a 100
 for (let i = 1; i <= 100; i++ ) {
     console.log(i);
     
-    listItem.innerHTML += i
+    if (i % 3 == 0 && i % 5 == 0) {
+        list.innerHTML += `<li class="redBox"> fizzBuzz </li>`;
+    } 
+    
+    else if ( i % 3 == 0) {
+        list.innerHTML += `<li class="greenBox"> fizz </li>`;
+    }
+    
+    else if ( i % 5 == 0) {
+        list.innerHTML += `<li class="yellowBox"> buzz </li>`;
+    } 
+    
+    else {
+        list.innerHTML += `<li class="blueBox"> ${i} </li>`;
+    }
     
 }
 
+list.insertAdjacentElement("beforebegin", title);
+title.innerHTML = "FizzBuzzDOM";
 
 
