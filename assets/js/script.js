ef4bd439bc7368c781ba7cab0eb44e53af691ac3
Vale2197 <valentinatrebbi97@gmail.com>
2021-10-13 15:56:35 +0200

    creati numeri a schermo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 /* Scrivi un programma che esegua un ciclo da 1 a 100 e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell'indice
 per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5. */
-const list = document.querySelector("numberList");
-
+const list = document.querySelector(".numberList");
 
 //Scrivi un programma che esegua un ciclo da 1 a 100
-for (let x = 1; x <= 100; x++ ) {
-    console.log(x);
-    list.innerHTML += x;
+for (let i = 1; i <= 100; i++ ) {
+    console.log(i);
+    
+    let listItem = `<li> ${i} </li>`;
+
+    list.innerHTML += listItem 
+    
 }
+
