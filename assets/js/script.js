1eb0b4fc87e964275584962508d787ae33b3047f
Vale2197 <valentinatrebbi97@gmail.com>
2021-10-13 16:00:10 +0200

    creata variabile costante con lista...problema

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 /* Scrivi un programma che esegua un ciclo da 1 a 100 e a ogni iterazione appenda un elemento html al container con uno stile differente a seconda del valore dell'indice
 per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5. */
 const list = document.querySelector(".numberList");
+const listItem = document.createElement("li");
+const numero = list.appendChild(listItem);
 
 //Scrivi un programma che esegua un ciclo da 1 a 100
 for (let i = 1; i <= 100; i++ ) {
     console.log(i);
     
-    let listItem = `<li> ${i} </li>`;
-
-    list.innerHTML += listItem 
+    listItem.innerHTML += i
     
 }
+
+
 
